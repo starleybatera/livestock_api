f1f23414e8b1f7835a25e666089ec2a7753e5c23
starleybatera <starley.batera@hotmail.com>
2021-01-17 11:31:14 -0300

    alteração nas tabelas de imagem

--- imagens/models.py
+++ imagens/models.py
@@ -5,7 +5,6 @@ def upload_imagem(instance, filename):
     return f"{instance.id}.{filename}"
 
 class Imagens(models.Model):
-    path = models.CharField(max_length=255)
     imagem = models.ImageField(upload_to=upload_imagem, blank=True, null=True)
     cod_animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
     
